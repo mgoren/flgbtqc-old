4aabc10cafe446dbb887f978d638ec4715a8d5da
Mike Goren <mike@mortalwombat.net>
2018-09-18 02:38:12 -0400

    finish adding minutes

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,17 +4,23 @@ $(document).ready(function() {
   $('#view-all').click(function() {
     if($(this).text() === 'View all') {
       $('.minute').addClass('well');
+      $('.minute').addClass('expanded');
+      $('.close').removeClass('hidden')
       $('.minute p').removeClass('hidden');
       $(this).text('Hide all')
     } else {
       $('.minute').removeClass('well');
+      $('.minute').removeClass('expanded');
+      $('.close').addClass('hidden')
       $('.minute p').addClass('hidden');
       $(this).text('View all')
     }
   });
 
-  $('.minute').click(function() {
-    $(this).toggleClass('well');
-    $(this).children('p').toggleClass('hidden');
+  $('.minute h6').click(function() {
+    $(this).parent().toggleClass('well');
+    $(this).parent().toggleClass('expanded')
+    $(this).children('.close').toggleClass('hidden')
+    $(this).parent().children('p').toggleClass('hidden');
   });
 })
