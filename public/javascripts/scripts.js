e4fb08a06c89652472c1fe2f816127d20f6a65f1
Mike Goren <mike@mortalwombat.net>
2018-09-21 16:53:34 -0700

    add epistles

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.minute p').addClass('hidden');
+  $('.minute ul').addClass('hidden');
 
   $('#view-all').click(function() {
     if($(this).text() === 'View all') {
@@ -7,12 +8,14 @@ $(document).ready(function() {
       $('.minute').addClass('expanded');
       $('.close').removeClass('hidden')
       $('.minute p').removeClass('hidden');
+      $('.minute ul').removeClass('hidden');
       $(this).text('Hide all')
     } else {
       $('.minute').removeClass('card card-body bg-light');
       $('.minute').removeClass('expanded');
       $('.close').addClass('hidden')
       $('.minute p').addClass('hidden');
+      $('.minute ul').addClass('hidden');
       $(this).text('View all')
     }
   });
@@ -22,5 +25,6 @@ $(document).ready(function() {
     $(this).parent().toggleClass('expanded')
     $(this).children('.close').toggleClass('hidden')
     $(this).parent().children('p').toggleClass('hidden');
+    $(this).parent().children('ul').toggleClass('hidden');
   });
 })
