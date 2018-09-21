f2ddbd35298b58a1afb330e7a882907023929366
Mike Goren <mike@mortalwombat.net>
2018-09-21 14:49:18 -0700

    add germantown minute and update styling

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 
   $('#view-all').click(function() {
     if($(this).text() === 'View all') {
-      $('.minute').addClass('well');
+      $('.minute').addClass('card card-body bg-light');
       $('.minute').addClass('expanded');
       $('.close').removeClass('hidden')
       $('.minute p').removeClass('hidden');
       $(this).text('Hide all')
     } else {
-      $('.minute').removeClass('well');
+      $('.minute').removeClass('card card-body bg-light');
       $('.minute').removeClass('expanded');
       $('.close').addClass('hidden')
       $('.minute p').addClass('hidden');
@@ -18,7 +18,7 @@ $(document).ready(function() {
   });
 
   $('.minute h6').click(function() {
-    $(this).parent().toggleClass('well');
+    $(this).parent().toggleClass('card card-body bg-light');
     $(this).parent().toggleClass('expanded')
     $(this).children('.close').toggleClass('hidden')
     $(this).parent().children('p').toggleClass('hidden');
